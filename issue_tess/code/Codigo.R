337020ddedf77b88e35a09954aacc30fe1fbc987
Francisco5674 <francisco.fuentes@uc.cl>
2024-10-11 20:46:41 -0300

    Revert "heatmap and trucks"
    This reverts commit 47eb76c6c3c3f4556316481bbed74f81c05dcdbe.

--- issue_tess/code/Codigo.R
+++ issue_tess/code/Codigo.R
@@ -2,28 +2,55 @@ library(data.table)
 library(h3jsr) 
 library(sf)
 library(lubridate)
+library(dplyr)
 
+# Set the seed for reproducibility
+set.seed(123)  # You can use any number
+setwd("C:/Users/Francisco/Desktop/Research/transport/issue_tess")
+
+# Data from Mati
+gps_mati <- fread("src/one_driver_september.csv")
+colnames(gps_mati) <- c("timestamp1","timestamp2","observation_id",
+                        "timestamp3","latitude","longitude","truck_id","x1","x2")
+
+# Set parameters and data
 gps_data <- fread("src/ArchivoCompleto.csv")
+gps_data$month <- format(gps_data$timestamp, "%m-%d")
+gps_data <- gps_data[gps_data$month == "08-02"]
+fwrite(gps_data, "src/gps_data_02082021.csv")
+gps_data <- fread("src/gps_data_02082021.csv")
 mobile_data <- fread("src/data_filtrada_02082021.csv")
+sample_size <- 300000
+hex_resolution <- 8
+
+# The program
+
+# sample
+#gps_ac <- gps_ac %>% slice_sample(n = sample_size)
+#gps_data <- gps_data %>% slice_sample(n = sample_size)
+gps_data[,reference_id:=NULL]
+mobile_data <- mobile_data %>% slice_sample(n = sample_size)
+
+# points in mobile data
 mobile_data$end_geo <- gsub("POINT\\(|\\)", "", mobile_data$end_geo)
 coords <- do.call(rbind, strsplit(mobile_data$end_geo, " "))
 mobile_data$longitude <- as.numeric(coords[, 1])
 mobile_data$latitude <- as.numeric(coords[, 2])
 mobile_data[,end_geo:=NULL]
 
-gps_data <- gps_data[1:100000]
-gps_data[,reference_id:=NULL]
-mobile_data <- mobile_data[1:1000000]
-
-hex_resolution <- 8
+# Tess
 gps_data$hex <- point_to_cell(matrix(c(gps_data$longitude,gps_data$latitude),ncol=2),res=hex_resolution)
 mobile_data$hex <- point_to_cell(matrix(c(mobile_data$longitude,mobile_data$latitude),ncol=2),res=hex_resolution)
-gps_data[,hora:=hour(timestamp)]
-mobile_data[,hora:=hour(end_absolutetime_ts)]
 
-joined_data <- gps_data[mobile_data, on = .(hex,hora), nomatch = 0,allow.cartesian=TRUE]
+#gps_data$date <- format(gps_data$timestamp, "%m-%d")
+gps_data$hour <- hour(gps_data$timestamp)
+
+#mobile_data$date <- format(mobile_data$end_absolutetime_ts, "%m-%d")
+mobile_data$hour <- hour(mobile_data$end_absolutetime_ts)
+
+joined_data <- gps_data[mobile_data, on = .(hex,hour), nomatch = 0,allow.cartesian=TRUE]
 head(joined_data)
 #contar personas, luego se puede segmentar por horarios o algo
 #ademas, se puede contar km por personas tambien
-conteo <- joined_data[,.N,by=vehicle_id]
-conteo <- joined_data[,.(length(unique(hashed_imsi))),by=vehicle_id]
+conteo <- joined_data[,.(npeople = uniqueN(c(hour,hashed_imsi))),by=vehicle_id]
+fwrite(conteo,"out/truck_people.csv")
---
+++ issue_tess/code/heat_map.r
@@ -0,0 +1,66 @@
+library(data.table)
+library(h3jsr) 
+library(sf)
+library(lubridate)
+library(dplyr)
+library(leaflet)  # For plotting
+
+setwd("C:/Users/Francisco/Desktop/Research/transport/issue_tess")
+
+# Opening data
+mobile_data <- fread("src/data_filtrada_04082021.csv")
+hex_resolution <- 8
+
+# Getting lat and long
+mobile_data$end_geo <- gsub("POINT\\(|\\)", "", mobile_data$end_geo)
+coords <- do.call(rbind, strsplit(mobile_data$end_geo, " "))
+mobile_data$longitude <- as.numeric(coords[, 1])
+mobile_data$latitude <- as.numeric(coords[, 2])
+mobile_data[,end_geo:=NULL]
+
+# Getting hours
+mobile_data$hour <- hour(mobile_data$end_absolutetime_ts)
+
+#### Assign H3 hexagons
+mobile_data$hex <- point_to_cell(
+  matrix(c(mobile_data$longitude,mobile_data$latitude),ncol=2),
+  res=hex_resolution)
+
+# Filter for the "peak_early" timeframe
+filter_time = 21
+mobile_data_filter <- mobile_data %>% filter(hour == filter_time)
+
+# Aggregating data by hexagon for "peak_early"
+hex_count <- mobile_data_filter %>%
+  group_by(hex) %>%
+  summarise(count = n())
+
+# Convert H3 hexagons to polygons (GeoJSON format) for mapping
+hex_polygons <- cell_to_polygon(hex_count$hex, simple = FALSE) %>%
+  st_as_sf()
+
+# Join the count data with the hex polygons
+hex_polygons$count <- hex_count$count
+
+# Apply logarithmic scaling to the counts (using log1p to handle zero counts)
+hex_polygons$log_count <- log1p(hex_polygons$count)
+
+# Create a leaflet heatmap for "peak_early" with logarithmic scale
+leaflet(hex_polygons) %>%
+  addProviderTiles("CartoDB.Positron") %>%  # Base map
+  addPolygons(
+    fillColor = ~colorNumeric("YlOrRd", log_count)(log_count),  # Logarithmic color scale
+    weight = 0.5,
+    opacity = 1,
+    color = 'white',
+    fillOpacity = 0.7,
+    smoothFactor = 0.2
+  ) %>%
+  addLegend(
+    "bottomright",
+    pal = colorNumeric("YlOrRd", hex_polygons$log_count),
+    values = hex_polygons$log_count,
+    title = paste("Log Count per Hex -",filter_time),
+    opacity = 1
+  )
+
